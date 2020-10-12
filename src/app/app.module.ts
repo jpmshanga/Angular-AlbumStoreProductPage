dd143a2a5ba7c6b370558108965c30f9e8c595f7
Joshua Mshanga <jpmshanga@gmail.com>
2020-10-12 21:53:19 +0300

    Completed module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { ProductTracklistingComponent } from './product-tracklisting/product-tra
 import { ProductListComponent } from './product-list/product-list.component';
 import {RouterModule, Routes} from '@angular/router';
 
-const appRoutes[]:Routes; 
+const appRoute:Routes=[]; 
 
 @NgModule({
   declarations: [
